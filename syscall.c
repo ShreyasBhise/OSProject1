efc543f28ab197c6c5333bbeeee6f0a699bd9573
Shreyas Y. Bhise <syb29@kill.cs.rutgers.edu>
2021-01-31 20:39:46 -0500

    Finished threads, halfway through syscall

--- syscall.c
+++ syscall.c
@@ -14,9 +14,24 @@
 #include<sys/syscall.h>
 
 double avg_time = 0;
+void syscall_benchmark() {
+	int numCalls = 3000, i;
+	long pid;
+	struct timeval start, end;
+	for(i = 0; i < numCalls; i++) {
+		gettimeofday(&start, NULL);
+		/*Syscall*/
+		pid = syscall(SYS_getpid);
+		gettimeofday(&end, NULL);
+		avg_time += (end.tv_usec - start.tv_usec);
+		avg_time += (end.tv_sec - start.tv_sec) * 1000;
+
+	}
+	avg_time /= numCalls;
+}
 
 int main(int argc, char *argv[]) {
-	/*Implement Code Here */
+	syscall_benchmark();
 
 	// Remember to place your final calculated average time
 	// per system call into the avg_time variable
