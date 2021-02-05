3e0329415c2d9981caef5aa857fbb8416fe5db72
Shreyas Y. Bhise <syb29@kill.cs.rutgers.edu>
2021-02-04 21:49:25 -0500

    fixed syscall

--- syscall.c
+++ syscall.c
@@ -14,25 +14,22 @@
 #include<sys/syscall.h>
 long syscall(long number);
 double avg_time = 0;
-void syscall_benchmark() {
+
+int main(int argc, char *argv[]) {
+
 	int numCalls = 3000, i;
 	long pid;
 	struct timeval start, end;
+	gettimeofday(&start, NULL);
 	for(i = 0; i < numCalls; i++) {
-		gettimeofday(&start, NULL);
 		/*Syscall*/
 		pid = syscall(SYS_getpid);
-		gettimeofday(&end, NULL);
-		avg_time += (end.tv_usec - start.tv_usec);
-		avg_time += (end.tv_sec - start.tv_sec) * 1000;
 
 	}
+	gettimeofday(&end, NULL);
+	avg_time += (end.tv_usec - start.tv_usec);
+	avg_time += (end.tv_sec - start.tv_sec) * 1000;
 	avg_time /= numCalls;
-}
-
-int main(int argc, char *argv[]) {
-	syscall_benchmark();
-
 	// Remember to place your final calculated average time
 	// per system call into the avg_time variable
 
